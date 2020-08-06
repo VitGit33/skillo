133f4357f3986d496c43155c27b77a29900590fd
Vitaliy Kulbaba <vitgit789@gmail.com>
2020-08-06 14:48:41 -0600

    second commit

--- src/ai/skillo/Main.java
+++ src/ai/skillo/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("Weight is: " + phone.weight);
         System.out.println("Year is: " + phone.year);
         System.out.println("Capacity is: " + phone.capacity + " GB");
+        System.out.println();
 
         phone.call("Whats up bro, are you going to run tomorrow morning?");
 
